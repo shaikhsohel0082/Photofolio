102094a6967897db98bbbed6eb50613a9220b69c
shaikhsohel0082 <shaikhsohel0082@gmail.com>
2024-04-20 19:58:12 +0530

    .env addded

--- src/FirebaseInit.js
+++ src/FirebaseInit.js
@@ -1,18 +1,18 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
-import { getStorage } from "firebase/storage";
+
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBunc5g8G4kBoDmOP9lB70fk_eNnsFdvro",
-  authDomain: "photofolio-baa8a.firebaseapp.com",
-  projectId: "photofolio-baa8a",
-  storageBucket: "photofolio-baa8a.appspot.com",
-  messagingSenderId: "521040769123",
-  appId: "1:521040769123:web:0b52c71252003130ee0856",
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
